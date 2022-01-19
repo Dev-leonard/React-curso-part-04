d71744eea139badb78e6dad0be8c3e617e4d7958
Leon Matias <rleonm@proveedor.intercorp.com.pe>
2022-01-19 17:55:46 -0500

    DefaultProps

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import PropsTypes from 'prop-types';
 //FC
-const PrimeraApp = ({saludo }) => {
+const PrimeraApp = ({ saludo, subtitulo }) => {
     // const saludo = 'Hola Mundo';
     // console.log(props)
     return (
         <>
             <h1>{saludo}</h1>
             {/*<pre>{JSON.stringify(saludo, null, 3)}</pre>;*/}
-            <p>Mi primera aplicacion</p>
+            <p>{ subtitulo }</p>
         </>
     );
 
 }
 PrimeraApp.propTypes = {
 saludo: PropsTypes.string.isRequired
+}
+PrimeraApp.defaultProps = {
+    subtitulo: 'soy un subtitulo'
 }
 export default PrimeraApp;
