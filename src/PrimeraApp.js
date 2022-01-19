f2673085d0d49ad276d1e6cd0e9be9b327ddca3e
Leon Matias <rleonm@proveedor.intercorp.com.pe>
2022-01-19 17:29:21 -0500

    impresion de variables en hHTML

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -2,9 +2,15 @@ import React from 'react';
 //FC
 const PrimeraApp = () => {
 
+    const saludo = {
+         nombre: 'edwin',
+          edad: 34
+    }
+
     return (
         <>
-            <h1>Hola Mundo</h1>;
+            {/*<h1>{saludo}</h1>*/}
+            <pre>{JSON.stringify(saludo, null, 3)}</pre>;
             <p>Mi primera aplicacion</p>
 
         </>
