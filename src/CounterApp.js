8b0b063bfda67937edd8af64c6c01d3cc2f12215
Leon Matias <rleonm@proveedor.intercorp.com.pe>
2022-01-19 18:40:02 -0500

    Evento click

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -1,15 +1,23 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const CounterApp = ({ value }) => {
+const CounterApp = ({value}) => {
+
+    //handleAdd
+    const handleAdd = (e) => {
+        console.log(e)
+
+    }
     return (
         <>
             <h1>CounterApp</h1>
             <h2>{value}</h2>
+
+            <button onClick={handleAdd}>+1</button>
         </>
     )
 }
 CounterApp.propTypes = {
-value: PropTypes.number
+    value: PropTypes.number
 }
 export default CounterApp;
